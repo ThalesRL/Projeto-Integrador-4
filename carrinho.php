c3373ebf955ed683b3b5ed1933dd456d6f903d66
ThalesRL <166050553+ThalesRL@users.noreply.github.com>
2025-11-30 19:08:39 -0300

    carrinho.php

---
+++ carrinho.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<title>Carrinho</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Carrinho de Compras</h1>
+<p>Seu carrinho está vazio no momento.</p>
+<button>Finalizar Compra</button>
+</body>
+</html>
