0eed346788c3335147019bb712a61b326ea890ce
ThalesRL <166050553+ThalesRL@users.noreply.github.com>
2025-11-30 19:07:58 -0300

    senha.php

---
+++ senha.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<title>Redefinir Senha</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Redefinir Senha</h1>
+<form>
+<label>Email cadastrado:</label>
+<input type="email" required>
+<button type="submit">Enviar link de redefinição</button>
+</form>
+</body>
+</html>
